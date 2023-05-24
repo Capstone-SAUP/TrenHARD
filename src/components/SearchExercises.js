c926b83dbdb1ab1ce9869ff93800638eb12e5820
aenonsantiago <aejorisantiago@gmail.com>
2023-05-25 02:26:27 +0800

    FE: Landing Page - Initial Build

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SearchExercises = () => {
     return (
-        <div>SearchExercises</div>
+        <div></div>
     )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
-import { Box } from '@mui/material';
+import React, { useState } from "react";
+import { Box } from "@mui/material";
 
-import Exercises from '../components/Execises';
-import SearchExercises from '../components/SearchExercises';
-import Herobanner from '../components/Herobanner';
+import Exercises from "../components/Execises";
+import SearchExercises from "../components/SearchExercises";
+import Herobanner from "../components/Herobanner";
 
 const Home = () => {
-    return (
-        <Box>
-        <Herobanner/>
-        <SearchExercises/>
-        <Exercises/>
-        </Box>
-    )
-}
+  return (
+    <Box>
+      <Herobanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  );
+};
 
 export default Home;
